975ee5c222dcc46703c891cc735d93588552812c
NathaliaLiberato <96542702+NathaliaLiberato@users.noreply.github.com>
2023-10-08 22:03:48 -0300

    Update conversao.js

---
+++ src/conversao.js
@@ -0,0 +1,12 @@
+{
+  "_id": {
+    "$oid": "651ac1770d4e79c18711a84b"
+  },
+  "Filme": "Sexta-Feira 13",
+  "Trilhas sonoras": [
+    "                   Sweet Dreams - Marilyn Manson\n\t\t   Highway To Hell - AC/DC\n\t\t   Ghostbusters - Ray Parker Jr\n\t\t   Anatomia - Maldita\n\t\t   Pet Sematary - Ramones\n\t\t   I'm Your Boogieman - Rob Zombie\n\t\t   First Date - Danko Jones\n\t\t   Mistérios da Meia-Noite - Zé Ramalho\n\t\t   Going Under - Evanescence\n\t\t   The Number Of The Beast - Iron Maiden\n\t\t   Black Sabbath - Black Sabbath\n\t\t   Sympathy For The Devil - The Rolling Stones\n\t\t   Thriller - Michael Jackson\n\n"
+  ],
+  "Ano": "2009",
+  "Genero": "Terror",
+  "Duraçao": "1h37m"
+}
