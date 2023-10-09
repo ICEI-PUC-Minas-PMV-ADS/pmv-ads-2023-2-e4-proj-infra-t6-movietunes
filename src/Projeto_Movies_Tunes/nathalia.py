d0fa1e642ed8c81634353773e68d3883a8ed0e66
fabio-furlan <furlan.fabio@hotmail.com>
2023-10-08 21:52:14 -0300

    aterando nome do arquivo bd realizado pela Natalia

--- src/Projeto_Movies_Tunes/nathalia.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-
-client = pymongo.MongoClient("mongodb://localhost", 27017)
-db = client["MovieTunes"]
-collection = db["Genero"]
-
-print(f"Essa é uma coleção usada {collection}")''
